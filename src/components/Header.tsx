e09c7ab87350c2627723e6366b5745f0bac27050
unknown <ryanzhangau@gmail.com>
2019-05-04 08:42:41 +1000

    load header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
+import '../style/header.scss';
 
 interface Option {
   label: string;
   value: string;
-}
-
-export interface HeaderProps {
-  onSelect(status: string): void;
 }
 
 const OptionList: Array<Option> = [
@@ -14,6 +11,9 @@ const OptionList: Array<Option> = [
   { label: 'Current', value: 'current' },
   { label: 'Sold', value: 'sold' }
 ];
+export interface HeaderProps {
+  onSelect(status: string): void;
+}
 
 const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
   const { onSelect } = props;
@@ -27,8 +27,13 @@ const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
   });
 
   return (
-    <header>
-      <select name='filter' id='filter' onChange={e => onSelect(e.target.value)}>
+    <header className='Header'>
+      <div className='Header_title'>Real Estate</div>
+      <select
+        className='Header_filter'
+        name='filter'
+        id='filter'
+        onChange={e => onSelect(e.target.value)}>
         {options}
       </select>
     </header>
