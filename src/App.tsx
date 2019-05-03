26e6afe28ec72384c078c2c0409354ebb8ecc807
Ryan Zhang <ryanzhangau@gmail.com>
2019-05-03 20:41:23 +1000

    add header and card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Card, { Item } from './components/Card';
 
 const App: React.FC = () => {
-  return <div className='App' />;
+  const [filter, setFilter] = useState('');
+  const [items, setItems] = useState([]);
+  
+  const updateFilter = (status: string) => {
+    setFilter(status);
+  };
+
+  useEffect(() => {
+    const getItems = async () => {
+      const response = await fetch('/challenge/properties', { mode: 'cors' });
+      const result = await response.json();
+      setItems(result);
+    };
+    getItems();
+  }, []);
+
+  const propertyList = () => {
+    let filteredItems = [...items];
+    if (filter) filteredItems = [...items.filter((i: Item) => i.status === filter)];
+    return filteredItems.map((p: Item) => {
+      return <Card item={p} key={p.id} />;
+    });
+  };
+
+  return (
+    <div className='App'>
+      <Header onSelect={updateFilter} />
+      {propertyList()}
+    </div>
+  );
 };
 
 export default App;
