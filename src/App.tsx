397be4e3242b66f054ac4be5a7f5cec5eb7a788d
unknown <ryanzhangau@gmail.com>
2019-05-04 14:51:28 +1000

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Card, { Item } from './components/Card';
+import Loading from './components/Loading';
+import './style/app.scss';
 
 const App: React.FC = () => {
   const [filter, setFilter] = useState('');
   const [items, setItems] = useState([]);
-  
+  const [isLoading, setIsLoading] = useState(true);
   const updateFilter = (status: string) => {
     setFilter(status);
   };
@@ -15,9 +17,16 @@ const App: React.FC = () => {
       const response = await fetch('/challenge/properties', { mode: 'cors' });
       const result = await response.json();
       setItems(result);
+      setIsLoading(false);
     };
     getItems();
   }, []);
+
+  const getFilter = (): Array<string> => {
+    const filterOp: Set<string> = new Set();
+    items.forEach((i: Item) => filterOp.add(i.status));
+    return Array.from(filterOp);
+  };
 
   const propertyList = () => {
     let filteredItems = [...items];
@@ -27,10 +36,17 @@ const App: React.FC = () => {
     });
   };
 
+  const loading = () => {
+    if (isLoading) return <Loading />;
+    return null;
+  };
+
   return (
     <div className='App'>
-      <Header onSelect={updateFilter} />
-      {propertyList()}
+      <Header onSelect={updateFilter} filter={getFilter} />
+      <div className='App_grid'>
+        {propertyList()} {loading()}
+      </div>
     </div>
   );
 };
