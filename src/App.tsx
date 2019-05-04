37fcbeaf87dc100f776e3e05f8f5aaa19d60e8d8
unknown <ryanzhangau@gmail.com>
2019-05-04 17:16:14 +1000

    load error message

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Card, { Item } from './components/Card';
 import Loading from './components/Loading';
+import ErrorMsg from './components/ErrorMsg';
 import './style/app.scss';
 
 const App: React.FC = () => {
   const [filter, setFilter] = useState('');
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState('');
   const updateFilter = (status: string) => {
     setFilter(status);
   };
-
-  useEffect(() => {
-    const getItems = async () => {
-      const response = await fetch('/challenge/properties', { mode: 'cors' });
+  const getItems = async () => {
+    setIsLoading(true);
+    setErr('');
+    try {
+      const response = await fetch('/challenge/propertiese', { mode: 'cors' });
       const result = await response.json();
       setItems(result);
-      setIsLoading(false);
-    };
+    } catch (e) {
+      console.log(e);
+      setErr('Cannot connect to server, please click reload button.');
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
     getItems();
   }, []);
 
@@ -41,11 +49,16 @@ const App: React.FC = () => {
     return null;
   };
 
+  const showErr = () => {
+    if (err) return <ErrorMsg error={err} onClick={getItems} />;
+    return null;
+  };
+
   return (
     <div className='App'>
       <Header onSelect={updateFilter} filter={getFilter} />
       <div className='App_grid'>
-        {propertyList()} {loading()}
+        {propertyList()} {loading()} {showErr()}
       </div>
     </div>
   );
